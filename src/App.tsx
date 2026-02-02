27e1666ad42734f0ae7394e0606b5ab3bfaea056
Chiziterem Eze <chiziteremoluchi5@gmail.com>
2026-02-03 00:01:23 +0100

    Implement form handling and validation with Zustand for ticket generation

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@
 import BackgroundImage from "./components/BackgroundImage"
 import TicketDesign from "./pages/ticketdesign/TicketDesign"
 import TicketForm from "./pages/ticketform/TicketForm"
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
         <BackgroundImage/>
-        <TicketForm/>
-        {/* <TicketDesign/> */}
-    </>
+        <Routes>
+          <Route path="/" element={<TicketForm/>}/>
+          <Route path="/ticket" element={<TicketDesign/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
